62a11d52b2c67942b1dff35ecf423fae6d7d5df6
Samuel Theofie <55137379+BlazedEngima@users.noreply.github.com>
2023-06-02 10:31:29 +0800

    Updated cuda.cu

--- src/cuda.cu
+++ src/cuda.cu
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <chrono>
+#include <tuple>
+
 #ifdef GUI
 #include <GL/glut.h>
 #include <GL/gl.h>
@@ -20,109 +22,112 @@ int block_size = 512;
 int n_body;
 int n_iteration;
 
-double total_time;
-
-
-__global__ void update_position(double *x, double *y, double *vx, double *vy, int n) {
-    //TODO: update position 
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < n) {
-        x[i] += vx[i]*dt;
-        y[i] += vy[i]*dt;
-
-        double r = sqrt(radius2);
-        if (x[i] <= r){
-            x[i] = r + err;
-            vx[i] = -vx[i];
-        }
-        else if (x[i] >= bound_x-r){
-            x[i] = bound_x - r - err;
-            vx[i] = -vx[i];
-        }
-        if (y[i] <= r){
-            y[i] = r + err;
-            vy[i] = -vy[i];
-        }
-        else if (y[i] >= bound_y-r){
-            y[i] = bound_y - r - err;
-            vy[i] = -vy[i];
-        } // handle wall collision
-    }
-}
-
-__global__ void update_velocity(double *m, double *x, double *y, double *vx, double *vy, int n) {
-    //TODO: calculate force and acceleration, update velocity
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < n) {
-        double x0 = x[i];
-        double y0 = y[i];
-        double vx0 = vx[i];
-        double vy0 = vy[i];
-        double ax = 0.0;
-        double ay = 0.0;
-
-        for (int j=0; j<n; j++){
-            if (i == j) continue;
-            double delta_x = x[i] - x[j];
-            double delta_y = y[i] - y[j];
-            double dist_s = delta_x*delta_x + delta_y*delta_y;
-            bool isCollision = false;
-            if (dist_s <= radius2*4){
-                dist_s = radius2*4;
-                isCollision = true;
-            }
-            double dist = sqrt(dist_s);
-
-            if (isCollision) {
-                double dot_prod = delta_x * (vx[i] - vx[j]) + delta_y * (vy[i] - vy[j]);
-                double value = 2 / (m[i] + m[j]) * dot_prod / dist_s;
-                vx0 -= value * delta_x * m[j];
-                vy0 -= value * delta_y * m[j];
-                
-                x0 += delta_x / dist * sqrt(radius2) / 2.0;
-                y0 += delta_y / dist * sqrt(radius2) / 2.0;
-            } else {
-                double F = m[i]*m[j]*gravity_const / dist_s;
-                ax -= F * delta_x / m[i];
-                ay -= F * delta_y / m[i];
-            }
-        }
-        vx[i] += ax * dt;
-        vy[i] += ay * dt;
-
-        double r = sqrt(radius2);
-        if (x[i] <= r){
-            x[i] = r + err;
-            vx[i] = -vx[i];
-        }
-        else if (x[i] >= bound_x-r){
-            x[i] = bound_x - r - err;
-            vx[i] = -vx[i];
-        }
-        if (y[i] <= r){
-            y[i] = r + err;
-            vy[i] = -vy[i];
-        }
-        else if (y[i] >= bound_y-r){
-            y[i] = bound_y - r - err;
-            vy[i] = -vy[i];
-        }
-    }
-}
-
-
 void generate_data(double *m, double *x,double *y,double *vx,double *vy, int n) {
-    // TODO: Generate proper initial position and mass for better visualization
+    // Generate proper initial position and mass for better visualization
     srand((unsigned)time(NULL));
     for (int i = 0; i < n; i++) {
-        m[i] = rand() % max_mass + 1.0f;
+        m[i] = rand() % (max_mass - min_mass) + min_mass;
+        // x[i] = rand() % bound_x;
+        // y[i] = rand() % bound_y;
+        
+        // For collision logic (fast paced)
         x[i] = 2000.0f + rand() % (bound_x / 4);
         y[i] = 2000.0f + rand() % (bound_y / 4);
+
+        // x[i] = 3000.0f + rand() % (bound_x / 4);
+        // y[i] = 3000.0f + rand() % (bound_y / 4);
+
         vx[i] = 0.0f;
         vy[i] = 0.0f;
     }
 }
 
+__global__ void update_position(double *x, double *y, double *vx, double *vy, int n) {
+    // Update position 
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) {
+        x[i] = x[i] + (vx[i] * dt);
+        y[i] = y[i] + (vy[i] * dt);
+
+        double r = sqrt(radius2);
+
+        // Border collision logic
+        if (x[i] <= r) {
+            x[i] = r + err;
+            vx[i] = -vx[i];
+        }
+        else if (x[i] >= bound_x-r) {
+            x[i] = bound_x - r - err;
+            vx[i] = -vx[i];
+        }
+        if (y[i] <= r) {
+            y[i] = r + err;
+            vy[i] = -vy[i];
+        }
+        else if (y[i] >= bound_y-r) {
+            y[i] = bound_y - r - err;
+            vy[i] = -vy[i];
+        }
+    }
+}
+
+__global__ void update_velocity(double *m, double *x, double *y, double *vx, double *vy, int n) {
+    // Calculate force and acceleration, update velocity
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) {  
+        double ax = 0;
+        double ay = 0;
+    
+        // Calculate force and acceleration, update velocity
+        for (size_t j = 0; j < n; j++) {
+            if (i == j) continue;
+
+            // Calculate the acceleration
+            double deltaX = x[i] - x[j];
+            double deltaY = y[i] - y[j];
+            double r_2 = (deltaX * deltaX) + (deltaY * deltaY);
+
+            bool collision = false;
+
+            // Detect collision
+            if (r_2 <= radius2) {
+
+                collision = true;
+                r_2 = radius2;
+            }
+
+            double r = sqrt(r_2);
+
+            // No collisions
+            if (!collision) {
+                double F = m[i]*m[j]*gravity_const / r_2; // calculate the force
+                ax -= (F / m[i]) * deltaX;
+                ay -= (F / m[i]) * deltaY;
+
+            // Collision with other bodies ignore collision force
+            } else {
+                // Conservation of momentum
+                double dot = deltaX * (vx[i] - vx[j]) + deltaY * (vy[i] - vy[j]);
+                double val = 2 / (m[i] + m[j]) * dot / r_2;
+
+                // Get velocity from the conservation of momentum
+                vx[i] -= val * deltaX * m[j];
+                vy[i] -= val * deltaY * m[j];
+
+                // We consider the volume of a body and reduce that from the positions
+                x[i] += deltaX / r * sqrt(radius2) / 2.0;
+                y[i] += deltaY / r * sqrt(radius2) / 2.0;
+            }
+        }
+    
+
+        // Update velocity arrays
+        vx[i] = vx[i] + (dt * ax);
+        vy[i] = vy[i] + (dt * ay);
+    
+        
+    }
+}
 
 void master() {
     double* m = new double[n_body];
@@ -133,7 +138,7 @@ void master() {
 
     generate_data(m, x, y, vx, vy, n_body);
 
-    Logger l = Logger("cuda", n_body, bound_x, bound_y);
+    Logger l = Logger("CUDA", n_body, bound_x, bound_y);
 
     double *device_m;
     double *device_x;
@@ -155,6 +160,8 @@ void master() {
 
     int n_block = n_body / block_size + 1;
 
+    std::chrono::duration<double> total{};
+
     for (int i = 0; i < n_iteration; i++){
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
@@ -166,9 +173,9 @@ void master() {
 
         std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> time_span = t2 - t1;
-        
-        printf("Iteration %d, elapsed time: %.3f\n", i, time_span);
-        total_time += time_span.count();
+        total += time_span;
+
+        // printf("Iteration %d, elapsed time: %.3f\n", i, time_span);
 
         l.save_frame(x, y);
 
@@ -199,6 +206,8 @@ void master() {
     cudaFree(device_vx);
     cudaFree(device_vy);
 
+    printf("Elapsed time: %.5f\n", total.count());
+
     delete[] m;
     delete[] x;
     delete[] y;
@@ -223,16 +232,13 @@ int main(int argc, char *argv[]){
     gluOrtho2D(0, bound_x, 0, bound_y);
     #endif
 
-    total_time = 0.0;
     master();
 
-    printf("Student ID: 119010437\n"); // replace it with your student id
-    printf("Name: ZHANG Shiyi\n"); // replace it with your name
+    printf("Student ID: 119010545\n");
+    printf("Name: Samuel Theofie\n");
     printf("Assignment 2: N Body Simulation CUDA Implementation\n");
-    printf("Total running time: %.4f\n",total_time);
+    printf("Number of bodies %d\n", n_body);
 
     return 0;
 
 }
-
-
